1615bacbacd4ea2d8a82a052d32ede99a7b398f4
lioneljuru <lioneljuru@gmail.com>
2023-12-06 20:51:43 +0200

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,5 +4,5 @@ def update_dictionary(a_dictionary, key, value):
     A function that replaces or adds
     key/value in a dictionary
     """
-    a_dictionary[key] = valie
+    a_dictionary[key] = value
     return a_dictionary
